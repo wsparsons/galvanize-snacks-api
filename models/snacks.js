2c5204d42e9b5be9520f3f98ac995ae33442ddde
Justin <justhays124@gmail.com>
2018-08-14 11:45:44 -0700

    index, show, create, and update functions for snacks working and throwing errors when needed

--- models/snacks.js
+++ models/snacks.js
@@ -5,5 +5,45 @@ function index() {
   return knex('snacks')
 }
 
+function getSnackById(id) {
+  return knex('snacks')
+    .where({ id })
+    .first()
+}
 
-module.exports = { index }
+function show(id) {
+  return getSnackById(id)
+    .then(snack => {
+      if (!snack) throw new Error('snacknotfound')
+      return snack
+    })
+}
+
+function create({ name, description, price, img, is_perishable }){
+  if(!name) throw new Error('snackNameWrong')
+  if(!description) throw new Error('snackDescriptionWrong')
+  if(!price) throw new Error('snackPriceWrong')
+  if(!img) throw new Error('snackImgWrong')
+  if(!is_perishable) throw new Error('snackPerishableWrong')
+  return knex('snacks')
+    .insert({ name, description, price, img, is_perishable })
+    .returning(['*'])
+}
+
+function update(id, body) {
+  return getSnackById(id)
+    .then(snack => {
+      if (!snack) throw new Error('snacknotfound')
+      return knex('snacks')
+        .where({ id })
+        .update(body)
+        .returning(['*'])    
+    })
+}
+
+function destroy() {
+  
+}
+
+
+module.exports = { index, show, create, update, destroy } 
