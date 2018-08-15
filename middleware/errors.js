1d7ca37b26dfbdfb8c82a5507988b7d83a7c41ad
Justin <justhays124@gmail.com>
2018-08-15 12:25:33 -0700

    error for missing update field(s)

--- middleware/errors.js
+++ middleware/errors.js
@@ -7,7 +7,7 @@ function processErrorMessage(err) {
         case 'snackPriceWrong': return { status: 400, message: 'Snack "price" must be a Float and is required'}
         case 'snackImgWrong': return { status: 400, message: 'Snack "img" must be a String and is required'}
         case 'snackPerishableWrong': return { status: 400, message: 'Snack "is_perishable" must be a Boolean and is required'}
-                  
+        case 'aFieldRequired': return { status: 400, message: 'At lease one(1) of the following fields is required: "name", "description", "price", "img", or "is_perishable"'}
         default:
           return { status: 500, message: 'An internal server error has occurred.' }
       }
