03a7014c9d5689af61a21a51c729da2eb7beaffd
Justin <justhays124@gmail.com>
2018-08-17 12:26:07 -0700

    refactored err

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   err = processErrorMessage(err)
   if (process.env.NODE_ENV !== 'test') console.error(err)
-  const status = err.status || 500
-  const message = err.message || 'Internal Error.'
+  const { status, message } = err
   res.status(status).json({ status, message })
 })
 
