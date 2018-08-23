388bac905058989332ca7e4f42f020ab7343a4ba
Justin <justhays124@gmail.com>
2018-08-23 14:18:04 -0700

    pulling in reviewsRoutes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
-const { snacksRoutes } = require('./routes')
+const { snacksRoutes, reviewsRoutes } = require('./routes')
 const processErrorMessage = require('./middleware/errors')
 require('dotenv').config()
 
@@ -14,6 +14,7 @@ app.use(morgan('dev'))
 app.use(bodyParser.json())
 
 app.use('/api/snacks', snacksRoutes)
+app.use('/api/snacks/:id/reviews', reviewsRoutes)
 
 app.use((req, res) => {
   const status = 404
